36286debc707579fee27ecc0f28e7efadf00b295
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-23 17:27:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
   configure do
     enable :sessions
     set :session_secret, "secret"
-  end
-
-  get '/' do
-    erb :index
   end
 
   post '/checkout' do
