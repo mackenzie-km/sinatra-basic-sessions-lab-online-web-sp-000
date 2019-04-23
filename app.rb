8f87b63166b4dcf1059e5b0991043e2590a29cd3
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-23 17:35:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, 'flatiron'
+  end
 
-  get '/index' do
+  get '/' do
     erb :'views/index'
   end
 
-  configure do
-    enable :sessions
-    set :session_secret, "secret"
-  end
+
 
   post '/checkout' do
     session["item"] = params[:item]
